521e622446841062b05ef7625d57e4a52c39eeb9
dolBlob <115646635+dolBlob@users.noreply.github.com>
2023-02-18 21:49:49 +0100

    Update debll.sh

--- debll.sh
+++ debll.sh
@@ -176,5 +176,6 @@ adb start-server && adb shell pm uninstall --user 0 com.miui.face
 adb start-server && adb shell pm uninstall --user 0 com.android.carrierdefaultapp
 adb start-server && adb shell pm uninstall --user 0 org.mipay.android.manager
 adb start-server && adb shell pm disable-user --user 0 com.android.managedprovisioning
+adb start-server && adb shell pm disable-user --user 0 com.android.shell
 adb start-server && adb shell pm disable-user --user 0 com.google.android.networkstack.tethering
 
